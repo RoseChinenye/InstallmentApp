8153fd874b27f8bdfa35f377285cf23be4824129
Chinenye <c.okeke@genesystechhub.com>
2022-11-25 09:47:18 +0100

    Added more exceptions handling

--- InstallmentRecordsApp/StoreItems.cs
+++ InstallmentRecordsApp/StoreItems.cs
@@ -22,6 +22,8 @@ namespace InstallmentRecordsApp
 
         public void AppStart()
         {
+            try 
+            { 
             Console.WriteLine("Welcome to Chy's Store!\n");
             Console.WriteLine("We have the following items...\n");
             Console.WriteLine(" 1. HP Laptop (#120,000)\n 2. iPhone  (#300,000)\n 3. Tablet  (#250,000)\n 4. SmartWatch  (#20,000)\n 5. None of these items\n ");
@@ -30,8 +32,8 @@ namespace InstallmentRecordsApp
 
             Console.WriteLine("Enter the quantity: ");
             _quantity = Convert.ToInt32(Console.ReadLine());
-
             
+
             Console.WriteLine("Enter your name: ");
             buyerName = Console.ReadLine();
             if (buyerName == "")
@@ -97,10 +99,17 @@ namespace InstallmentRecordsApp
                     Console.WriteLine("Please, Enter the correct Item Number!");
                     break;
             }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void installmentPlans() 
         {
+            try
+            { 
             Console.WriteLine(" 1. Daily ........... 10% payment.\n " +
                 "2. Weekly ........... 15% payment.\n " +
                 "3. Bi-Weekly ........... 20% payment.\n " +
@@ -131,10 +140,17 @@ namespace InstallmentRecordsApp
                     yearly();
                     break;
             }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void daily() 
         {
+            try 
+            { 
             _percentage = (decimal)(1d / 100); //10% for daily installment plan
             Console.WriteLine("You choosed Daily installment plan!\n");
             Console.WriteLine("Press any key to continue...");
@@ -167,12 +183,18 @@ namespace InstallmentRecordsApp
             Console.WriteLine("Congratulations!!!! Payment Completed!");
 
 
-
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
 
         public void weekly() 
         {
+            try
+            { 
             _percentage = (decimal)(15d / 100); //15% for weekly installment plan
             Console.WriteLine("You choosed Weekly installment plan!\n");
             Console.WriteLine("Press any key to continue...");
@@ -203,12 +225,17 @@ namespace InstallmentRecordsApp
 
             Console.WriteLine();
             Console.WriteLine("Congratulations!!!! Payment Completed!");
-
-
         }
+        catch (Exception e) 
+        {
+            Console.WriteLine(e.Message);
+        }
+
+}
 
         public void biWeekly()
         {
+            try { 
             _percentage = (decimal)(20d / 100); //20% for bi-weekly installment plan
             Console.WriteLine("You choosed bi-weekly installment plan!\n");
             Console.WriteLine("Press any key to continue...");
@@ -240,11 +267,16 @@ namespace InstallmentRecordsApp
             Console.WriteLine();
             Console.WriteLine("Congratulations!!!! Payment Completed!");
 
-
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void monthly()
         {
+            try { 
             _percentage = (decimal)(25d / 100); //25% for monthly installment plan
             Console.WriteLine("You choosed monthly installment plan!\n");
             Console.WriteLine("Press any key to continue...");
@@ -275,11 +307,16 @@ namespace InstallmentRecordsApp
             Console.WriteLine();
             Console.WriteLine("Congratulations!!!! Payment Completed!");
 
-
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void sixMonths()
         {
+            try { 
             _percentage = (decimal)(30d / 100); //30% for six months installment plan
             Console.WriteLine("You choosed six months installment plan!\n");
             Console.WriteLine("Press any key to continue...");
@@ -311,11 +348,16 @@ namespace InstallmentRecordsApp
             Console.WriteLine();
             Console.WriteLine("Congratulations!!!! Payment Completed!");
 
-
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void yearly()
         {
+            try { 
             _percentage = (decimal)(35d / 100); //35% for yearly installment plan
             Console.WriteLine("You choosed one year installment plan!\n");
             Console.WriteLine("Press any key to continue...");
@@ -346,7 +388,11 @@ namespace InstallmentRecordsApp
             Console.WriteLine();
             Console.WriteLine("Congratulations!!!! Payment Completed!");
 
-
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
 
